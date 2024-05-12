f8b77f2724143bdec9b700fd67e28508abf6b2b0
Velizar Ganchev <velizar_ganchev@hotmail.com>
2024-05-12 08:19:41 +0200

    change the name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'koch-welt';
+  title = 'my-recipe-book';
 
 }
--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -2,13 +2,19 @@ import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, MaybeAsync, Resolve, RouterStateSnapshot } from "@angular/router";
 import { Recipe } from "./recipe.model";
 import { DataStorageService } from "../data-storage.service";
+import { RecipesService } from "./recipes.service";
 
 @Injectable({ providedIn: 'root' })
 export class RecipesResolverService implements Resolve<Recipe[]> {
 
-    constructor(private dataStorageService: DataStorageService) { }
+    constructor(private dataStorageService: DataStorageService, private recipeService: RecipesService) { }
 
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): MaybeAsync<Recipe[]> {
-        return this.dataStorageService.fetchRecipes();
+        const recipes = this.recipeService.getAllRecipes();
+        if (recipes.length === 0) {
+            return this.dataStorageService.fetchRecipes();
+        } else {
+            recipes;
+        }
     }
 }
